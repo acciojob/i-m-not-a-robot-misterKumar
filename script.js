4f02db04d4d446358693f369d841a53882ea6d90
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-14 14:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,90 @@
+let image = [];
+for (let i = 0; i < 5; i++) {
+  let t = document.createElement("IMG");
+  t.setAttribute("data-ns-test", `img${i + 1}`);
+  t.width = 100;
+  t.height = 100;
+  t.onclick = (e) => captchaClick(e);
+  t.src = `images/${i + 1}.jpg`;
+  image.push(t);
+}
+let temp = Math.floor(Math.random() * 5);
+let t = document.createElement("IMG");
+t.setAttribute("data-ns-test", `img${temp + 1}`);
+t.width = 100;
+t.height = 100;
+t.onclick = (e) => captchaClick(e);
+t.src = `images/${temp + 1}.jpg`;
+image.push(t);
+image.sort(() => Math.random() - 0.5);
+for (let i = 0; i < 6; i++) {
+  document.getElementById("main").appendChild(image[i]);
+}
 
-let n = Math.floor(Math.random()%5 + 1);
-let main = document.getElementsByTagName('main')[0];
-let images = document.querySelectorAll('img');
-var randomImage = document.getElementsByClassName('none')[0];
-let imageContainer = document.getElementsByClassName("image")[0];
-let arr = [];
-let reset = document.getElementById('reset');
-reset.style.display = 'none';
-let verify = document.getElementById('verify');
-let result = document.getElementsByClassName('para')[0];
-verify.style.display = 'none';
-randomImage.setAttribute("class", `img${n}`);
-images.forEach((e)=>{
-  e.addEventListener('click', ()=>{
-    e.style.border = "8px solid blue";
-    arr.push(e.getAttribute('class'));
-    if(arr.length>0 && arr.length<3){
-      reset.style.display = 'block';
-    }
-    if(arr.length==2){
-      verify.style.display = 'block';
-    }
-    if(arr.length>2){
-      reset.style.display = 'none';
-      verify.style.display = 'none';
-      let para = document.createElement('p');
-      para.innerText = "Please select only 2 images. Kindly refresh the page";
-      main.append(para);
-    }
-  })
-})
-
-reset.addEventListener('click',()=>{
-  for(let i = 0;i<=arr.length;i++){
-    arr.pop();
+let captcha = [];
+function clearCaptcha() {
+  // console.log("Clearing captcha");
+  for (let i = 0; i < 6; i++) {
+    image[i].onclick = (e) => captchaClick(e);
   }
-  reset.style.display = "none";
-  result.innerText = "";
-  verify.style.display = 'none'
-  images.forEach((e)=>{
-      e.style.border = 'none';
-  })
-})
+  captcha = [];
+  try {
+    document.getElementById("para").remove();
+  } catch (e) {}
+  try {
+    document.getElementById("btn").remove();
+  } catch (e) {}
+  try {
+    document.getElementById("reset").remove();
+  } catch (e) {}
+}
 
-verify.addEventListener('click',()=>{
-  if(arr[0]==arr[1]){
-     result.innerText = 'You are a human. Congratulations!.';
-  }else{
-    result.innerText = "We can't verify you as a human. You selected the non-identical tiles.";
+function captchaClick(e) {
+  console.log(e.target.attributes["data-ns-test"].nodeValue);
+  captcha.push(e.target.attributes["data-ns-test"].nodeValue);
+  e.target.onclick = () => {};
+  // console.log(captcha);
+
+  if (captcha.length === 1) {
+    let p = document.createElement("button");
+    p.id = "reset";
+    p.innerHTML = "Reset";
+    p.onclick = () => {
+      clearCaptcha();
+    };
+    document.getElementById("main").appendChild(p);
   }
-})
 
-imageContainer.append(randomImage);
+  if (captcha.length === 2) {
+    let t = document.createElement("button");
+    t.id = "btn";
+    t.innerHTML = "Verify";
+    t.onclick = () => {
+      captchaVerify();
+    };
+    document.getElementById("main").appendChild(t);
+  } else if (captcha.length > 2) {
+    try {
+      document.getElementById("btn").remove();
+    } catch (e) {}
+  }
+  try {
+    document.getElementById("para").remove();
+  } catch (e) {}
+}
+
+function captchaVerify() {
+  if (captcha.length === 2 && captcha[0] === captcha[1]) {
+    let t = document.createElement("P");
+    t.innerHTML = "You are a human. Congratulations!";
+    t.id = "para";
+    document.getElementById("main").appendChild(t);
+  } else {
+    let t = document.createElement("P");
+    t.innerHTML =
+      "We can't verify you as a human. You selected the non-identical tiles.";
+    t.id = "para";
+    document.getElementById("main").appendChild(t);
+  }
+  document.getElementById("btn").remove();
+}
